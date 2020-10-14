78f66750ad1f9f8bf64a8efdc087e78e0a59654b
ChunLin Diao <chunlin.diao@dxc.com>
2020-10-14 15:25:20 +0800

    d

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ print("sdf")
 print(n
       d
 # connection_strings = "mysql+pymysql://USERNAME:PASSWORD@IP_ADDRESS_OF_MYSQL_SERVER:3306/DATABASE_NAME"
+
+      # amqp_url = 'amqp://guest:guest@localhost:5672/%2F'
